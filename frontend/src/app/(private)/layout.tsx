92510504b858f31dcb02bc93e60051a5be697a47
Nuan59 <lnwlion555@gmail.com>
2026-05-24 15:43:55 +0700

    fix: responsive layout for all screen sizes

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -14,13 +14,13 @@ export default function PrivateLayout({
 	return (
 		<>
 			<OrderProvider>
-				<div className={`h-[calc(100%-96px)] bg-slate-200 ${prompt.className}`}>
+				<div className={`min-h-screen bg-slate-200 ${prompt.className}`}>
 					<Navbar />
 					<main className="grid grid-cols-[3fr_1fr] text-slate-900 gap-0 mt-0">
-						<section className="m-10 bg-slate-50 font-bold p-10 h-[80vh] rounded-xl pt-5">
+						<section className="m-4 xl:m-8 bg-slate-50 font-bold p-4 xl:p-8 min-h-[calc(100vh-80px)] max-h-[calc(100vh-80px)] overflow-y-auto rounded-xl">
 							{children}
 						</section>
-						<aside>
+						<aside className="sticky top-0 h-screen overflow-y-auto">
 							<OrderCard />
 						</aside>
 					</main>
