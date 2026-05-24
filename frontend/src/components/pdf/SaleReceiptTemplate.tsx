d05a3d4bc2289694028958a110eefb35768939b9
Nuan59 <lnwlion555@gmail.com>
2026-05-24 15:11:01 +0700

    fix: chassi field name in receipt

--- frontend/src/components/pdf/SaleReceiptTemplate.tsx
+++ frontend/src/components/pdf/SaleReceiptTemplate.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Document, Page, Text, View, Image } from "@react-pdf/renderer";
 import { IOrder } from "@/types/Order";
@@ -206,7 +205,7 @@ const ReceiptPage: React.FC<{
             <Text style={styles.col4}></Text><Text style={styles.col5}></Text>
           </View>
           <View style={styles.tableRow}>
-            <Text style={styles.col1}>  หมายเลขตัวถัง {sanitizeText(bike?.chassis || '-')}{ZWJ}</Text>
+            <Text style={styles.col1}>  หมายเลขตัวถัง {sanitizeText(bike?.chassi || '-')}{ZWJ}</Text>
             <Text style={styles.col2}></Text><Text style={styles.col3}></Text>
             <Text style={styles.col4}></Text><Text style={styles.col5}></Text>
           </View>
